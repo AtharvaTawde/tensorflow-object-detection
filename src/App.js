2ae0740ded6efffc96474714dcecf73e4a1cb35b
Atharva Tawde <atharvatawde894@gmail.com>
2021-06-03 11:12:44 -0700

    Refactored Tensorflow.js import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useRef, useState, useEffect } from "react";
-import * as tf from "@tensorflow/tfjs";
+import React, { useRef, useEffect } from "react";
+import "@tensorflow/tfjs";
 import * as cocossd from "@tensorflow-models/coco-ssd";
 
 import Webcam from "react-webcam";
